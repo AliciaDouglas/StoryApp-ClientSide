0b4d6967c4ed2600b22c64b4468c9ff9f7257dd8
Alicia <aliciadouglas26@gmail.com>
2018-10-08 19:36:30 -0400

    set up basic login and register pages and set up error state matcher service

--- StoryApp/src/app/material.ts
+++ StoryApp/src/app/material.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatFormFieldModule, MatError} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
     imports: [
-        MatMenuModule
+        MatMenuModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatError
     ],
     exports: [
-        MatMenuModule
+        MatMenuModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatError
     ],
   })
   export class Material { }
